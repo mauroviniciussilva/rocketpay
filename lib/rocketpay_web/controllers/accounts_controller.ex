b232e0bd820dc3802e0d054e59a0c578a3c6bebe
Mauro Oliveira <mauroviniciussilva@gmail.com>
2021-02-24 14:56:33 -0300

    adding accounts and creating the deposit API

---
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -0,0 +1,19 @@
+defmodule RocketpayWeb.AccountsController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Account
+
+  action_fallback RocketpayWeb.FallbackController
+
+  def deposit(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+    end
+  end
+
+  def withdraw(conn, params) do
+
+  end
+end
